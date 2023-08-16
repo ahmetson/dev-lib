c524172666b7428b9c72db0c20a29bc6f5c6599a
Medet Ahmetson <admin@blocklords.io>
2023-08-16 17:37:48 +0300

    More clarifying comments to the config package

--- config/type.go
+++ config/type.go
@@ -1,3 +1,4 @@
+// Package config defines the specific parameters of the Contexts and Dev Context
 package config
 
 type ContextType = string
@@ -5,7 +6,6 @@ type ContextType = string
 const (
 	// DevContext indicates that all dependency proxies are in the local machine
 	DevContext ContextType = "development"
-	// DefaultContext indicates that all dependencies are in any machine.
-	// It's unspecified.
+	// DefaultContext indicates that the context is unspecified.
 	DefaultContext ContextType = "default"
 )
