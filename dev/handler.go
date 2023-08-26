6b491ab902b575049b54e7de0689926ca235e898
Medet Ahmetson <ahmetson@zoho.com>
2023-08-26 16:57:32 +0400

    context calls the dep manager's services

--- dev/handler.go
+++ dev/handler.go
@@ -12,6 +12,7 @@ package dev
 import (
 	"fmt"
 	"github.com/ahmetson/client-lib"
+	clientConfig "github.com/ahmetson/client-lib/config"
 	"github.com/ahmetson/common-lib/data_type/key_value"
 	"github.com/ahmetson/common-lib/message"
 	"github.com/ahmetson/handler-lib/config"
@@ -25,37 +26,28 @@ const (
 	CtxManagerUrl = "inproc://context_handler" // only one context per
 )
 
-// onClose closing all the dependencies in the orchestra.
-func (ctx *Context) onClose(request message.Request, logger *log.Logger, _ ...*client.Socket) message.Reply {
-	logger.Info("closing the orchestra",
-		"orchestra type", ctx.Type(),
-		"todo", "close all dependencies if any",
-		"todo", "close the main service",
-		"goal", "exit the application")
-
-	//for _, dep_manager := range ctx.deps {
-	//if dep_manager.cmd == nil || dep_manager.cmd.Process == nil {
-	//	continue
-	//}
-
-	// I expect that the killing process will release its resources as well.
-	//err := dep_manager.cmd.Process.Kill()
-	//if err != nil {
-	//	logger.Error("dep_manager.cmd.Process.Kill", "error", err, "dep_manager", dep_manager.Url(), "command", "onClose")
-	//	return request.Fail(fmt.Sprintf(`dep_manager("%s").cmd.Process.Kill: %v`, dep_manager.Url(), err))
-	//}
-	//logger.Info("dependency was closed", "url", dep_manager.Url())
-	//}
-
-	err := ctx.closeService(logger)
+// onClose closing single the dependency in the orchestra.
+func (ctx *Context) onClose(request message.Request) message.Reply {
+	kv, err := request.Parameters.GetKeyValue("dep")
 	if err != nil {
-		return request.Fail(fmt.Sprintf("orchestra.closeServer: %v", err))
+		return request.Fail(fmt.Sprintf("request.Parameters.GetKeyValue('dep'): %v", err))
 	}
+
+	var c *clientConfig.Client
+	err = kv.Interface(c)
+	if err != nil {
+		return request.Fail(fmt.Sprintf("key_value.KeyValue('dep').Interface(): %v", err))
+	}
+
+	err = ctx.depManager.Close(c)
+	if err != nil {
+		return request.Fail(fmt.Sprintf("ctx.depManager.Close(): %v", err))
+	}
+
 	// since we closed the service, for the orchestra the service is not ready.
 	// the service should call itself
 	ctx.serviceReady = false
 
-	logger.Info("dependencies were closed, service received a message to be closed as well")
 	return request.Ok(key_value.Empty())
 }
 
@@ -96,12 +88,20 @@ func (ctx *Context) Run(logger *log.Logger) error {
 	return nil
 }
 
-// Close sends a close signal to the orchestra.
-func (ctx *Context) Close(logger *log.Logger) error {
+// Close sends a close signal to the dependency through dependency manager
+//
+// todo move out from the handler
+func (ctx *Context) Close(c *clientConfig.Client, logger *log.Logger) error {
 	if ctx.controller == nil {
 		logger.Warn("skipping, since orchestra.ControllerCategory is not initialised", "todo", "call orchestra.Run()")
 		return nil
 	}
+
+	kv, err := key_value.NewFromInterface(c)
+	if err != nil {
+		return fmt.Errorf("key_value.NewFromInterface: %w", err)
+	}
+
 	contextClient, err := client.NewRaw(zmq4.REP, CtxManagerUrl)
 	if err != nil {
 		logger.Error("client.NewReq", "error", err)
@@ -110,7 +110,7 @@ func (ctx *Context) Close(logger *log.Logger) error {
 
 	closeRequest := &message.Request{
 		Command:    "close",
-		Parameters: key_value.Empty(),
+		Parameters: key_value.Empty().Set("dep", kv),
 	}
 
 	if err := contextClient.Submit(closeRequest); err != nil {
