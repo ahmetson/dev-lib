6f8bc2b29c9852d72c4cd82d571043269a9a61f3
Medet Ahmetson <ahmetson@zoho.com>
2023-09-03 20:53:53 +0400

    add dev handler

---
+++ dep_handler/dev.go
@@ -0,0 +1,242 @@
+// Package dep_handler creates a thread that manages the dependencies
+package dep_handler
+
+import (
+	"fmt"
+	clientConfig "github.com/ahmetson/client-lib/config"
+	"github.com/ahmetson/common-lib/data_type/key_value"
+	"github.com/ahmetson/common-lib/message"
+	"github.com/ahmetson/dev-lib/base/dep_manager"
+	"github.com/ahmetson/dev-lib/dep"
+	"github.com/ahmetson/handler-lib/base"
+	handlerConfig "github.com/ahmetson/handler-lib/config"
+	"github.com/ahmetson/handler-lib/replier"
+	"github.com/ahmetson/log-lib"
+)
+
+const (
+	Category     = "dep_handler"   // handler category
+	DepInstalled = "dep-installed" // the command to check is dependency installed
+	DepRunning   = "dep-running"   // the command to check is dependency running
+	InstallDep   = "install-dep"   // the command to install the dependency
+	RunDep       = "run-dep"       // the command to run the dependency
+	UninstallDep = "uninstall-dep" // the command to remove the dependency binary. if possible, then remove the source code as well.
+	CloseDep     = "close-dep"     // the command to stop the running dependency
+)
+
+type DepHandler struct {
+	handler base.Interface
+	manager dep_manager.Interface
+	logger  *log.Logger
+}
+
+// ServiceConfig returns the socket configuration of the handler
+func ServiceConfig() *handlerConfig.Handler {
+	return handlerConfig.NewInternalHandler(handlerConfig.ReplierType, Category)
+}
+
+// New dep handler returned
+func New(manager dep_manager.Interface) (*DepHandler, error) {
+	handler := replier.New()
+
+	logger, err := log.New("dep-handler", true)
+	if err != nil {
+		return nil, fmt.Errorf("log.New('dep-handler'): %w", err)
+	}
+
+	handler.SetConfig(ServiceConfig())
+	err = handler.SetLogger(logger)
+	if err != nil {
+		return nil, fmt.Errorf("handler.SetLogger: %w", err)
+	}
+
+	return &DepHandler{
+		manager: manager,
+		handler: handler,
+		logger:  logger,
+	}, nil
+}
+
+// onDepInstalled checks whether the dependency installed or not.
+// Requires the 'url' of the dependency.
+//
+// Returns 'installed' boolean parameter
+func (h *DepHandler) onDepInstalled(req message.Request) message.Reply {
+	url, err := req.Parameters.GetString("url")
+	if err != nil {
+		return req.Fail(fmt.Sprintf("req.Parameters.GetString('url'): %v", err))
+	}
+
+	installed := h.manager.Installed(url)
+	params := key_value.Empty().Set("installed", installed)
+	return req.Ok(params)
+}
+
+// onDepRunning checks whether the dependency is running or not.
+// Requires 'dep' of the clientConfig.Client.
+//
+// Returns 'running' boolean result
+func (h *DepHandler) onDepRunning(req message.Request) message.Reply {
+	kv, err := req.Parameters.GetKeyValue("dep")
+	if err != nil {
+		return req.Fail(fmt.Sprintf("req.Parameters.GetKeyValue('client'): %v", err))
+	}
+
+	var c clientConfig.Client
+	err = kv.Interface(&c)
+	if err != nil {
+		return req.Fail(fmt.Sprintf("kv.Interface: %v", err))
+	}
+
+	c.UrlFunc(handlerConfig.ExternalUrlByClient)
+
+	running, err := h.manager.Running(&c)
+	if err != nil {
+		return req.Fail(fmt.Sprintf("h.manager.Running: %v", err))
+	}
+
+	params := key_value.Empty().Set("running", running)
+	return req.Ok(params)
+}
+
+// onInstallDep installs the dependency. if it comes with the source code, then build that as well.
+//
+// Requires the 'src' of a dep.Src type, returns nothing.
+//
+// todo create a publisher that publishes the result of the installation, so user won't wait until installation.
+func (h *DepHandler) onInstallDep(req message.Request) message.Reply {
+	kv, err := req.Parameters.GetKeyValue("src")
+	if err != nil {
+		return req.Fail(fmt.Sprintf("req.Parameters.GetKeyValue('client'): %v", err))
+	}
+
+	var src dep.Src
+	err = kv.Interface(&src)
+	if err != nil {
+		return req.Fail(fmt.Sprintf("kv.Interface: %v", err))
+	}
+
+	err = h.manager.Install(&src, h.logger)
+	if err != nil {
+		return req.Fail(fmt.Sprintf("h.manager.Install: %v", err))
+	}
+
+	return req.Ok(key_value.Empty())
+}
+
+// onRunDep runs the dependency.
+// Requires 'url' string parameter, 'id' string parameter, and the 'parent' of the clientConfig.Client type.
+// Returns nothing.
+//
+// todo make it publish the result through publisher, so user won't wait for the result.
+func (h *DepHandler) onRunDep(req message.Request) message.Reply {
+	kv, err := req.Parameters.GetKeyValue("parent")
+	if err != nil {
+		return req.Fail(fmt.Sprintf("req.Parameters.GetKeyValue('parent'): %v", err))
+	}
+
+	var parent clientConfig.Client
+	err = kv.Interface(&parent)
+	if err != nil {
+		return req.Fail(fmt.Sprintf("kv.Interface: %v", err))
+	}
+
+	parent.UrlFunc(clientConfig.Url)
+
+	url, err := req.Parameters.GetString("url")
+	if err != nil {
+		return req.Fail(fmt.Sprintf("req.Parameters.GetString('url'): %v", err))
+	}
+
+	id, err := req.Parameters.GetString("id")
+	if err != nil {
+		return req.Fail(fmt.Sprintf("req.Parameters.GetString('id'): %v", err))
+	}
+
+	err = h.manager.Run(url, id, &parent, h.logger)
+	if err != nil {
+		return req.Fail(fmt.Sprintf("h.manager.Run(url: '%s', id: '%s'): %v", url, id, err))
+	}
+
+	return req.Ok(key_value.Empty())
+}
+
+// onUninstallDep uninstalls the dependency binary. if it comes with the source code, then deletes source code as well.
+//
+// Requires the 'src' of a dep.Src type, returns nothing.
+//
+// todo creates a publisher that publishes the result of the installation, so user won't wait until installation.
+func (h *DepHandler) onUninstallDep(req message.Request) message.Reply {
+	kv, err := req.Parameters.GetKeyValue("src")
+	if err != nil {
+		return req.Fail(fmt.Sprintf("req.Parameters.GetKeyValue('client'): %v", err))
+	}
+
+	var src dep.Src
+	err = kv.Interface(&src)
+	if err != nil {
+		return req.Fail(fmt.Sprintf("kv.Interface: %v", err))
+	}
+
+	err = h.manager.Uninstall(&src)
+	if err != nil {
+		return req.Fail(fmt.Sprintf("h.manager.Uninstall: %v", err))
+	}
+
+	return req.Ok(key_value.Empty())
+}
+
+// onCloseDep stops the dependency.
+// Requires 'dep' of the clientConfig.Client type.
+// Returns nothing.
+//
+// todo make it publish the result through publisher, so user won't wait for the result.
+func (h *DepHandler) onCloseDep(req message.Request) message.Reply {
+	kv, err := req.Parameters.GetKeyValue("dep")
+	if err != nil {
+		return req.Fail(fmt.Sprintf("req.Parameters.GetKeyValue('dep'): %v", err))
+	}
+
+	var c clientConfig.Client
+	err = kv.Interface(&c)
+	if err != nil {
+		return req.Fail(fmt.Sprintf("kv.Interface: %v", err))
+	}
+
+	c.UrlFunc(clientConfig.Url)
+
+	err = h.manager.Close(&c)
+	if err != nil {
+		return req.Fail(fmt.Sprintf("h.manager.Close: %v", err))
+	}
+
+	return req.Ok(key_value.Empty())
+}
+
+func (h *DepHandler) Close() error {
+	return h.handler.Close()
+}
+
+// Start the dependency handler with the available operations.
+func (h *DepHandler) Start() error {
+	if err := h.handler.Route(DepInstalled, h.onDepInstalled); err != nil {
+		return fmt.Errorf("h.handler.Route('%s'): %v", DepInstalled, err)
+	}
+	if err := h.handler.Route(DepRunning, h.onDepRunning); err != nil {
+		return fmt.Errorf("h.handler.Route('%s'): %v", DepRunning, err)
+	}
+	if err := h.handler.Route(InstallDep, h.onInstallDep); err != nil {
+		return fmt.Errorf("h.handler.Route('%s'): %v", InstallDep, err)
+	}
+	if err := h.handler.Route(RunDep, h.onRunDep); err != nil {
+		return fmt.Errorf("h.handler.Route('%s'): %v", RunDep, err)
+	}
+	if err := h.handler.Route(UninstallDep, h.onUninstallDep); err != nil {
+		return fmt.Errorf("h.handler.Route('%s'): %v", UninstallDep, err)
+	}
+	if err := h.handler.Route(CloseDep, h.onCloseDep); err != nil {
+		return fmt.Errorf("h.handler.Route('%s'): %v", CloseDep, err)
+	}
+
+	return h.handler.Start()
+}
---
+++ dep_handler/dev_test.go
@@ -0,0 +1,267 @@
+package dep_handler
+
+import (
+	"fmt"
+	"github.com/ahmetson/client-lib"
+	clientConfig "github.com/ahmetson/client-lib/config"
+	"github.com/ahmetson/common-lib/data_type/key_value"
+	"github.com/ahmetson/common-lib/message"
+	"github.com/ahmetson/dev-lib/dep"
+	"github.com/ahmetson/dev-lib/dep_manager"
+	handlerConfig "github.com/ahmetson/handler-lib/config"
+	"github.com/ahmetson/log-lib"
+	"github.com/ahmetson/os-lib/path"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// Define the suite, and absorb the built-in basic suite
+// functionality from testify - including a T() method which
+// returns the current testing orchestra
+type TestDepHandlerSuite struct {
+	suite.Suite
+
+	logger     *log.Logger
+	dep        *DepHandler          // the manager to test
+	currentDir string               // executable to store the binaries and source codes
+	url        string               // dependency source code
+	id         string               // the id of the dependency
+	parent     *clientConfig.Client // the info about the service to which dependency should connect
+
+	client *client.Socket // imitating the service
+}
+
+// Make sure that Account is set to five
+// before each test
+func (test *TestDepHandlerSuite) SetupTest() {
+	s := test.Suite.Require
+
+	logger, _ := log.New("test", false)
+	test.logger = logger
+
+	currentDir, err := path.CurrentDir()
+	s().NoError(err)
+	test.currentDir = currentDir
+
+	srcPath := path.AbsDir(currentDir, "_sds/src")
+	binPath := path.AbsDir(currentDir, "_sds/bin")
+
+	// Make sure that the folders don't exist. They will be added later
+	manager := dep_manager.New()
+	err = manager.SetPaths(srcPath, binPath)
+	s().NoError(err)
+
+	test.dep, err = New(manager)
+	s().NoError(err)
+
+	// Start the handler
+	s().NoError(test.dep.Start())
+
+	// wait a bit for closing
+	time.Sleep(time.Millisecond * 100)
+
+	// A valid source code that we want to download
+	test.url = "github.com/ahmetson/test-manager"
+
+	test.id = "test-manager"
+	test.parent = &clientConfig.Client{
+		ServiceUrl: "dev-lib",
+		Id:         "parent",
+		Port:       120,
+		TargetType: handlerConfig.SocketType(handlerConfig.ReplierType),
+	}
+
+	config := ServiceConfig()
+	socketType := handlerConfig.SocketType(config.Type)
+	socket, err := client.NewRaw(socketType, fmt.Sprintf("inproc://%s", config.Id))
+	s().NoError(err)
+
+	test.client = socket
+	test.client.Timeout(time.Second * 30)
+	test.client.Attempt(1)
+}
+
+func (test *TestDepHandlerSuite) TearDownTest() {
+	s := test.Suite.Require
+
+	s().NoError(test.client.Close())
+
+	s().NoError(test.dep.Close())
+
+	// Wait a bit for the close of the handler thread.
+	time.Sleep(time.Millisecond * 100)
+}
+
+// Test_10_Install checks InstallDep and DepInstalled
+// This is the first part of Install.
+// The second part of Install is building.
+//
+// Tests DepManager.downloadSrc and srcExist.
+func (test *TestDepHandlerSuite) Test_10_Install() {
+	s := test.Suite.Require
+
+	src, err := dep.New(test.url)
+	s().NoError(err)
+
+	// installation must fail since nothing installed
+	req := message.Request{
+		Command:    DepInstalled,
+		Parameters: key_value.Empty().Set("url", test.url),
+	}
+	rep, err := test.client.Request(&req)
+	s().NoError(err)
+	s().True(rep.IsOK())
+	res, err := rep.Parameters.GetBoolean("installed")
+	s().NoError(err)
+	s().False(res)
+
+	// There should be a source code
+	installReq := message.Request{
+		Command:    InstallDep,
+		Parameters: key_value.Empty().Set("src", src),
+	}
+	rep, err = test.client.Request(&installReq)
+	s().NoError(err)
+	s().True(rep.IsOK())
+
+	// wait a bit until its installed
+	time.Sleep(time.Millisecond * 100)
+
+	//
+	// Testing the installed after installation
+	//
+	rep, err = test.client.Request(&req)
+	s().NoError(err)
+	s().True(rep.IsOK())
+	res, err = rep.Parameters.GetBoolean("installed")
+	s().NoError(err)
+	s().True(res)
+}
+
+// Test_11_Uninstall deletes the binary and source code installed at Test_11_Install
+func (test *TestDepHandlerSuite) Test_11_Uninstall() {
+	s := test.Suite.Require
+
+	src, err := dep.New(test.url)
+	s().NoError(err)
+
+	// The binary must be installed to uninstall
+	req := message.Request{
+		Command:    DepInstalled,
+		Parameters: key_value.Empty().Set("url", test.url),
+	}
+	rep, err := test.client.Request(&req)
+	s().NoError(err)
+	s().True(rep.IsOK())
+	res, err := rep.Parameters.GetBoolean("installed")
+	s().NoError(err)
+	s().True(res)
+
+	// Uninstall
+	uninstallReq := message.Request{
+		Command:    UninstallDep,
+		Parameters: key_value.Empty().Set("src", src),
+	}
+	rep, err = test.client.Request(&uninstallReq)
+	s().NoError(err)
+	s().True(rep.IsOK())
+
+	// wait a bit for effect
+	time.Sleep(time.Millisecond * 100)
+
+	// After uninstallation, we should not have the binary
+	rep, err = test.client.Request(&req)
+	s().NoError(err)
+	s().True(rep.IsOK())
+	res, err = rep.Parameters.GetBoolean("installed")
+	s().NoError(err)
+	s().False(res)
+}
+
+// Test_13_Run tests DepRunning, RunDep and CloseDep commands.
+func (test *TestDepHandlerSuite) Test_13_Run() {
+	s := test.Suite.Require
+
+	depClient := &clientConfig.Client{
+		ServiceUrl: "test-manager",
+		Id:         test.id,
+		Port:       6000,
+		TargetType: handlerConfig.SocketType(handlerConfig.ReplierType),
+	}
+
+	src, err := dep.New(test.url)
+	s().NoError(err)
+	src.SetBranch("server") // the sample server is written in this branch.
+
+	// First, install the dependency
+	installReq := message.Request{
+		Command:    InstallDep,
+		Parameters: key_value.Empty().Set("src", src),
+	}
+	rep, err := test.client.Request(&installReq)
+	s().NoError(err)
+	s().True(rep.IsOK())
+
+	// Let's run it
+	runReq := message.Request{
+		Command: RunDep,
+		Parameters: key_value.Empty().
+			Set("parent", test.parent).
+			Set("url", src.Url).
+			Set("id", test.id),
+	}
+	rep, err = test.client.Request(&runReq)
+	s().NoError(err)
+	s().True(rep.IsOK())
+
+	// Just wait a bit for initialization of the service
+	time.Sleep(time.Millisecond * 100)
+
+	// check that service is running
+	runningReq := message.Request{
+		Command: DepRunning,
+		Parameters: key_value.Empty().
+			Set("dep", depClient),
+	}
+	running, err := test.client.Request(&runningReq)
+	s().NoError(err)
+	s().True(running.IsOK())
+	result, err := running.Parameters.GetBoolean("running")
+	s().NoError(err)
+	s().True(result)
+
+	// Close the service
+	closeReq := message.Request{
+		Command: CloseDep,
+		Parameters: key_value.Empty().
+			Set("dep", depClient),
+	}
+	running, err = test.client.Request(&closeReq)
+	s().NoError(err)
+	s().True(running.IsOK())
+
+	// Wait a bit for closing the dep process
+	time.Sleep(time.Millisecond * 100)
+
+	// Checking for a running dep after it was closed must fail
+	running, err = test.client.Request(&runningReq)
+	s().NoError(err)
+	s().True(running.IsOK())
+	result, err = running.Parameters.GetBoolean("running")
+	s().NoError(err)
+	s().False(result)
+
+	// Clean out the installed files
+	installReq.Command = UninstallDep
+	rep, err = test.client.Request(&installReq)
+	s().NoError(err)
+	s().True(rep.IsOK())
+}
+
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestDepManager(t *testing.T) {
+	suite.Run(t, new(TestDepHandlerSuite))
+}
